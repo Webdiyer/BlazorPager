49da48cf4c015063921218f7342dc3230c89297f
Webdiyer <webdiyer@gmail.com>
2019-03-11 22:05:19 +0800

    Added samples to the BlazorHosted project (not working, to be fixed)...

--- Demo/BlazorHosted/BlazorHosted.Server/Controllers/SampleDataController.cs
+++ Demo/BlazorHosted/BlazorHosted.Server/Controllers/SampleDataController.cs
@@ -1,6 +1,8 @@
 ﻿using BlazorHosted.Shared;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.IO;
 using System.Linq;
 using X.PagedList;
 
@@ -9,6 +11,12 @@ namespace BlazorHosted.Server.Controllers
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
+        public SampleDataController(IHostingEnvironment env)
+        {
+            Env = env;
+        }
+        private IHostingEnvironment Env;
+
         private static string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -26,6 +34,23 @@ namespace BlazorHosted.Server.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             }).ToPagedList(pageIndex, pageSize);
             return new PagedListData<WeatherForecast> { Items = data, MetaData = data.GetMetaData() };
+        }
+
+        [HttpGet("[action]")]
+        public PagedListData<Order> Orders(string companyName,int pageIndex, int pageSize)
+        {
+            var path = Path.Combine(Env.ContentRootPath, "orders.json");
+            var ods = Newtonsoft.Json.JsonConvert.DeserializeObject<Order[]>(System.IO.File.ReadAllText(path));
+            IPagedList<Order> orders;
+            if (!string.IsNullOrWhiteSpace(companyName))
+            {
+                orders = ods.Where(o => o.CompanyName.Contains(companyName)).ToPagedList(pageIndex, pageSize);
+            }
+            else
+            {
+                orders = ods.ToPagedList(pageIndex, pageSize);
+            }
+            return new PagedListData<Order> { Items = orders, MetaData = orders.GetMetaData() };
         }
     }
 }
---
+++ Demo/BlazorHosted/BlazorHosted.Shared/Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorHosted.Shared
+{
+    public class Order
+    {
+
+        public int OrderID
+        {
+            get;set;
+        }
+
+        public DateTime OrderDate
+        {
+            get; set;
+        }
+
+
+        public string CompanyName
+        {
+            get; set;
+        }
+
+        public string EmployeeName
+        {
+            get; set;
+        }
+
+        public string CustomerID
+        {
+            get; set;
+        }
+    }
+}
